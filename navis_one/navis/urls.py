5406318ae178cd03091a0558efe45fb94e0f29de
AlexGr33n <greentr33@gmail.com>
2021-08-25 15:46:43 +0300

    add smth

---
+++ navis_one/navis/urls.py
@@ -0,0 +1,8 @@
+from django.apps import apps
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+
+]
--- navis_one/navis/views.py
+++ navis_one/navis/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView, View
 
-# Create your views here.
+from .models import *
+
+
