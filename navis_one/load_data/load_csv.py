472310926a52f035f562165a58992601186a9da5
AlexGr33n <greentr33@gmail.com>
2021-08-26 16:35:26 +0300

    add load_data

---
+++ navis_one/load_data/load_csv.py
@@ -0,0 +1,69 @@
+import base64
+from zeep import Client, Settings
+from requests import Session
+from requests.auth import HTTPBasicAuth
+from zeep.transports import Transport
+
+
+class LoadData:
+    def __init__(self):
+        session = Session()
+        session.auth = HTTPBasicAuth('Robot', 'Robot')
+        transport = Transport(session=session, timeout=600)
+        settings = Settings(xml_huge_tree=True)
+        self.client = Client('http://192.168.75.115:8005/live/ws/navis?wsdl', transport=transport, settings=settings)
+
+    def load_products(self):
+        products = self.client.service.GetData('products')
+        data = base64.b64decode(products)
+        file = open('cache/products.csv', 'w', newline='', encoding='utf-8')
+        file.write(str(data.decode('utf-8')))
+        file.close()
+
+    def load_cross(self):
+        cross = self.client.service.GetData('cross')
+        data = base64.b64decode(cross)
+        file = open('cache/cross.csv', 'w', newline='', encoding='utf-8')
+        file.write(str(data.decode('utf-8')))
+        file.close()
+
+    def load_description(self):
+        description = self.client.service.GetData('description')
+        data = base64.b64decode(description)
+        file = open('cache/description.csv', 'w', newline='', encoding='utf-8')
+        file.write(str(data.decode('utf-8')))
+        file.close()
+
+    def load_applicability(self):
+        applicability = self.client.service.GetData('applicability')
+        data = base64.b64decode(applicability)
+        file = open('cache/applicability.csv', 'w', newline='', encoding='utf-8')
+        file.write(str(data.decode('utf-8')))
+        file.close()
+
+    def load_product_manufacturer_model(self):
+        deficit = self.client.service.GetData('product_manufacturer_model')
+        data = base64.b64decode(deficit)
+        file = open('cache/product_manufacturer_model.csv', 'w', newline='', encoding='utf-8')
+        file.write(str(data.decode('utf-8')))
+        file.close()
+
+    def load_product_images(self):
+        deficit = self.client.service.GetData('product_images')
+        data = base64.b64decode(deficit)
+        file = open('cache/product_images.csv', 'w', newline='', encoding='utf-8')
+        file.write(str(data.decode('utf-8')))
+        file.close()
+
+
+LoadDataProducts = LoadData()
+LoadDataProducts.load_products()
+LoadDataProducts.load_cross()
+LoadDataProducts.load_description()
+LoadDataProducts.load_applicability()
+LoadDataProducts.load_product_manufacturer_model()
+LoadDataProducts.load_product_images()
+print('Load Data Products')
+
+
+
--- navis_one/navis/models.py
+++ navis_one/navis/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from datetime import datetime
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=500)
+    url = models.SlugField(max_length=150, unique=True)
+
+    def __str__(self):
+        return str(self.url)
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.url})
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=500)
+    url = models.SlugField(max_length=150, unique=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs={'int': self.id})
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+
+class ProductImage(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    source_url = models.CharField(max_length=300, null=True, blank=True)
+    image_url = models.URLField(max_length=300, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = "ProductImage"
+        verbose_name_plural = "ProductImages"
 
 
