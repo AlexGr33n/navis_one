e0b753e2055af60b6a5c333c219e87349d1a4237
AlexGr33n <greentr33@gmail.com>
2021-09-03 13:11:15 +0300

    models --

--- navis_one/navis/admin.py
+++ navis_one/navis/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django import forms
+from modeltranslation.admin import TranslationAdmin
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import *
+
+admin.site.site_title = 'Navis.one'
+admin.site.site_header = 'Navis.one'
+
+
+class ContentAdminForm(forms.ModelForm):
+    full_text_ru = forms.CharField(widget=CKEditorUploadingWidget())
+    full_text_uk = forms.CharField(widget=CKEditorUploadingWidget())
+    full_text_en = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Content
+        fields = '__all__'
+
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(ContentCategory)
+admin.site.register(Content)
+
+
--- navis_one/navis/translation.py
+++ navis_one/navis/translation.py
@@ -1,1 +1,22 @@
 from modeltranslation.translator import register, TranslationOptions
+from .models import Product, ContentCategory, Category, Content
+
+
+@register(Category)
+class CategoryTranslationOptions(TranslationOptions):
+    fields = ('name')
+
+
+@register(Product)
+class ProductTranslationOptions(TranslationOptions):
+    fields = ('name')
+
+
+@register(ContentCategory)
+class CatalogCategoryTranslationOptions(TranslationOptions):
+    fields = ('name')
+
+
+@register(Content)
+class ContentTypeMethodTranslationOptions(TranslationOptions):
+    fields = ('title', 'full_text')
