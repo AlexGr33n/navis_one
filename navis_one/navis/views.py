fd0ae03bc355dac1cd4632a6fdb81e2caca937c4
AlexGr33n <greentr33@gmail.com>
2021-09-06 15:54:44 +0300

    view +-

--- navis_one/navis/views.py
+++ navis_one/navis/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, View
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import *
 
@@ -14,3 +15,22 @@ def about(request):
 
 def contact(request):
     return render(request, 'navis_one/contact.html')
+
+
+def product_list(request, category_slug):
+    categories = Category.objects.all()
+    category = get_object_or_404(Category, url=category_slug)
+    products = Product.objects.filter(category_id=category)
+    paginator = Paginator(products, 30)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj, 'category': category, 'categories': categories, 'products': products
+    }
+    return render(request, 'navis_one/product/product_list.html', context)
+
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    context = {'product': product}
+    return render(request, 'navis_one/product/product_detail.html', context)
